44e8b66634d1bb3cf468fca57d2332567234cda5
r2coder <r2coder.163.com>
2025-09-21 23:15:57 +0800

    03(completed)

--- exercises/03_ticket_v1/09_heap/src/lib.rs
+++ exercises/03_ticket_v1/09_heap/src/lib.rs
@@ -13,7 +13,8 @@ mod tests {
 
     #[test]
     fn string_size() {
-        assert_eq!(size_of::<String>(), todo!());
+        // pointer, length, capacity 3 个 usize(64 / 8 * 3)
+        assert_eq!(size_of::<String>(), 24);
     }
 
     #[test]
@@ -23,6 +24,6 @@ mod tests {
         // but, in general, the memory layout of structs is a more complex topic.
         // If you're curious, check out the "Type layout" section of The Rust Reference
         // https://doc.rust-lang.org/reference/type-layout.html for more information.
-        assert_eq!(size_of::<Ticket>(), todo!());
+        assert_eq!(size_of::<Ticket>(), 72);
     }
 }
